1a84b866b676ba0b53605301f97fab90ae507feb
tc0dev <104026747+tc0dev@users.noreply.github.com>
2024-05-26 10:55:01 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Kurulum
+
+önce kur.cmd'yi sonrasında discorddjsvoice.cmd
+
+run.bat
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "",
+    channel: "1244054843317030956",
+    staff: "1244188477298573383", 
+    ses: ""
+}
